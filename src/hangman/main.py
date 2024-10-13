a3c271f15e2da8c9de9a9c366dea24eb0b5f906a
ikorepanov <ikorepanov@gmail.com>
2024-10-13 16:21:07 +0700

    refactor according to flake8, mypy

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -1,151 +1,139 @@
 import re
-from io import TextIOWrapper
 from random import randrange
 
 from paths import DICT_PATH
 
+WELCOME_MESSAGE = """
+ _____________________________________________________________
+|                                                             |
+|                                                             |
+|              Вас приветствует игра "Виселица!               |
+|                                                             |
+|_____________________________________________________________|
+"""
+
+EMPTY = """
+         ____
+        |    |
+             |
+             |
+             |
+    _________|_____
+"""
+HEAD = """
+         ____
+        |    |
+        O    |
+             |
+             |
+    _________|_____
+"""
+BODY = """
+         ____
+        |    |
+        O    |
+        |    |
+             |
+    _________|_____
+"""
+R_HAND = """
+         ____
+        |    |
+      __O    |
+        |    |
+             |
+    _________|_____
+"""
+L_HAND = """
+         ____
+        |    |
+      __O__  |
+        |    |
+             |
+    _________|_____
+"""
+R_LEG = """
+         ____
+        |    |
+      __O    |
+        |    |
+       /     |
+    _________|_____
+"""
+L_LEG = """
+         ____
+        |    |
+      __O__  |
+        |    |
+       / \   |
+    _________|_____
+"""
+
+options = {
+    '0': EMPTY,
+    '1': HEAD,
+    '2': BODY,
+    '3': R_HAND,
+    '4': L_HAND,
+    '5': R_LEG,
+    '6': L_LEG,
+}
+
 
 def get_random_word(
-        fhand: TextIOWrapper,
-        default: str | None,
+    default: str | None,
 ) -> str:
-    word = default
-    for i, aline in enumerate(fhand, start=1):
-        if randrange(i) == 0:
-            word = aline
-    if word is not None:
-        return word.strip()
-    else:
+    with DICT_PATH.open() as fhand:
+        word = default
+        for index, aline in enumerate(fhand, start=1):
+            if randrange(index) == 0:
+                word = aline
+        if word is not None:
+            return word.strip()
         return ''
 
 
 def open_mask(
-        mask: str,
-        word: str,
-        letter: str,
+    mask: str,
+    word: str,
+    letter: str,
 ) -> str:
     mask_asterisks = list(mask)
-    indices = [i for i, char in enumerate(word) if char == letter]
+    indices = [index for index, char in enumerate(word) if char == letter]
     for index in indices:
         mask_asterisks[index] = letter
     return ''.join(mask_asterisks)
 
 
 def build_hangman(mistakes: int) -> str:
-    if mistakes == 0:
-        return """
-             ____
-            |    |
-                 |
-                 |
-                 |
-        _________|_____
-        """
-    elif mistakes == 1:
-        return """
-             ____
-            |    |
-            O    |
-                 |
-                 |
-        _________|_____
-        """
-    elif mistakes == 2:
-        return """
-             ____
-            |    |
-            O    |
-            |    |
-                 |
-        _________|_____
-        """
-    elif mistakes == 3:
-        return """
-             ____
-            |    |
-          __O    |
-            |    |
-                 |
-        _________|_____
-        """
-    elif mistakes == 4:
-        return """
-             ____
-            |    |
-          __O__  |
-            |    |
-                 |
-        _________|_____
-        """
-    elif mistakes == 5:
-        return """
-             ____
-            |    |
-          __O__  |
-            |    |
-           /     |
-        _________|_____
-        """
-    elif mistakes == 6:
-        return """
-             ____
-            |    |
-          __O__  |
-            |    |
-           / \\   |
-        _________|_____
-        """
-    else:
-        return ''
-
-
-def is_cyrillic(char: str) -> bool:
-    return bool(re.fullmatch('[ёа-я]', char))
-
-
-def first_time(
-        letters: list[str],
-        letter: str,
-) -> bool:
-    if letter not in letters:
-        return True
-    else:
-        return False
-
-
-def print_welcome_message() -> None:
-    print(
-            ' _____________________________________________________________\n'
-            '|                                                             |\n'
-            '|                                                             |\n'
-            '|              Вас приветствует игра "Виселица!               |\n'
-            '|                                                             |\n'
-            '|_____________________________________________________________|\n'
-        )
+    action = options.get(str(mistakes))
+    if action:
+        return action
+    return ''
 
 
 def show_current_state(
-        mask: str,
-        mistakes: int,
+    mask: str,
+    mistakes: int,
 ) -> None:
     print(
         ' '.join(mask),
-        '\n\n'
-        f'Количество ошибок: {mistakes}\n'
-        f'{build_hangman(mistakes)}'
+        f'\n\nКоличество ошибок: {mistakes}\n',
+        f'{build_hangman(mistakes)}',
     )
 
 
 def enter_letter(
-        mask: str,
-        mistakes: int,
-        used_letters: list[str],
+    mask: str,
+    mistakes: int,
+    used_letters: list[str],
 ) -> str:
     show_current_state(mask, mistakes)
 
     while True:
         letter = input('Введите букву: ')
-        if not is_cyrillic(letter):
+
+        if bool(not re.fullmatch('[ёа-я]', letter)):
             print(
                 '\nНеобходимо использовать - только - буквы',
                 'русского алфавита в нижнем регистре: а - я',
@@ -153,32 +141,30 @@ def enter_letter(
                 end='',
             )
             continue
-        elif not first_time(used_letters, letter):
+
+        elif letter in used_letters:
+            list_used_letters = ', '.join(used_letters)
             print(
                 '\nВы уже вводили, в том числе, эту букву:',
-                f'{", ".join(used_letters)}',
+                f'{list_used_letters}',
                 '\033[2F\033[K',
                 end='',
             )
             continue
-        else:
-            used_letters.append(letter)
-            print('\033[12F\033[J', end='')
-            return letter
+
+        used_letters.append(letter)
+        print('\033[12F\033[J', end='')
+
+        return letter
 
 
-def prepare_screen(game_count: int) -> None:
+def run_game(game_count: int) -> None:
     if game_count == 0:
         print('\033[2F\033[J', end='')
     else:
         print('\033[16F\033[J', end='')
 
-
-def run_game(game_count: int) -> None:
-    prepare_screen(game_count)
-
-    with DICT_PATH.open() as f:
-        word = get_random_word(f, default=None)
+    word = get_random_word(default=None)
 
     mask = '*' * len(word)
     mistakes = 0
@@ -203,14 +189,11 @@ def run_game(game_count: int) -> None:
 
 
 def main() -> None:
-    print_welcome_message()
+    print(WELCOME_MESSAGE)
     game_count = 0
 
     while True:
-        decision = input(
-            'Начать новую игру (1) или выйти из приложения(2)? '
-            'Введите 1 или 2:\n'
-        )
+        decision = input('Начать новую игру (1) или выйти из приложения(2)? Введите 1 или 2:\n')
 
         if decision == '1':
             run_game(game_count)
@@ -218,7 +201,7 @@ def main() -> None:
 
         elif decision == '2':
             print('\033[K\033[EВсего доброго!\n')
-            exit()
+            break
 
         else:
             print('(Нужно ввести 1 или 2)\033[F\033[K\033[F', end='')
